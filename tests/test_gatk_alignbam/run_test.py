d154482a4267362807fb69742e9593b9f05b5ae6
Paul Cao <paulcao@broadinstitute.org>
2018-03-22 19:59:04 -0400

    Check in fix for alignbam bug.

--- tests/test_gatk_alignbam/run_test.py
+++ tests/test_gatk_alignbam/run_test.py
@@ -6,16 +6,13 @@ import subprocess
 def test_verify_valid_comparison_dir(comparison_dir):
 
     def bam_read_count(fname):
-        p = subprocess.Popen(['samtools view -F 0x40', fname, '| cut -f1 | sort | uniq | wc -l'], shell=True)
-        #p = subprocess.Popen(['samtools', 'view', '-F', '0x40', fname, '|', 'cut', '-f1', '|', 'sort', '|', 'uniq',
-        #                      '|', 'wc', '-l'], stdout=subprocess.PIPE,
-        #                                      stderr=subprocess.PIPE)
-        result, err = p.communicate()
+        p = subprocess.Popen(['samtools view -F 0x40 ' + fname + ' | cut -f1 | sort | uniq | wc -l'], shell=True,
+                             stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        lines = ""
+        for line in p.stdout:
+            lines = lines + line
 
-        if p.returncode != 0:
-            raise IOError(err)
-
-        return int(result.strip().split()[0])
+        return int(lines.strip().split()[0])
 
     GATK_GOAL_PATH = "/cil/shed/resources/wdl/gatk/alignbam/output/CandidaAuris.bam"
     COMPARISON_PATH= comparison_dir + "/CandidaAuris.bam"
