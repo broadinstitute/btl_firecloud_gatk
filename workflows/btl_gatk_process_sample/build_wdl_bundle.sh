6f5adb39262c5355e3d656fa778bef5df2498768
gsaksena <gsaksena@broadinstitute.org>
2018-03-27 06:59:41 -0400

    add process_sample, wdl validates

---
+++ workflows/btl_gatk_process_sample/build_wdl_bundle.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+zip    -j  wdl_bundle.zip ../btl_gatk_alignbam/taskdef.btl_gatk_alignbam.wdl ../btl_gatk_tcir/taskdef.btl_gatk_tcir.wdl ../btl_gatk_bqsr/taskdef.btl_gatk_bqsr.wdl ../btl_gatk_haplotypecaller/taskdef.btl_gatk_haplotypecaller.wdl  
+
