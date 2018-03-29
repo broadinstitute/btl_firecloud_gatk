686056857a5be8d53534147d21b619b9bf20d1e1
Paul Cao <paulcao@broadinstitute.org>
2018-03-29 14:44:00 -0400

    Check in tests for Process Cohorts and Process Sample test.

---
+++ tests/test_gatk_process_sample/run_test.py
@@ -0,0 +1,21 @@
+import pytest
+import tarfile
+import glob
+import subprocess
+
+def test_verify_valid_comparison_dir(comparison_dir):
+
+    def word_count(fname):
+        p = subprocess.Popen(['wc', '-l', fname], stdout=subprocess.PIPE,
+                                              stderr=subprocess.PIPE)
+        result, err = p.communicate()
+
+        if p.returncode != 0:
+            raise IOError(err)
+
+        return int(result.strip().split()[0])
+
+    GATK_GOAL_PATH = "/cil/shed/resources/wdl/gatk/process_sample/output/Candida_Auris.gvcf"
+    COMPARISON_PATH= comparison_dir + "/Candida_Auris.gvcf"
+
+    assert(word_count(GATK_GOAL_PATH) == word_count(COMPARISON_PATH))
